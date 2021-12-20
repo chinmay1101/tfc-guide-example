2171299eb1a65797dbf2cfb91ea647ec26da6660
chinmay1101 <68799466+chinmay1101@users.noreply.github.com>
2021-12-20 18:18:56 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,19 +1,1 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.90.0"
-    }
 
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
-    }
-  }
-
-  required_version = ">= 0.14.0"
-}
-
-provider "azurerm" {
-  features {}
-}
